354e06115c88232edb661d00e4e4949e687aa46f
493472322@qq.com <2410602>
2018-07-27 20:29:54 +0800

    Initial commit

---
+++ app/src/main/java/rocksea/com/git/MainActivity.kt
@@ -0,0 +1,12 @@
+package rocksea.com.git
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
